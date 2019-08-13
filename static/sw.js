07ca5e624a24f6478cf5d9e796c97d646c6e9fba
Ricardo Moreira <ricardo.d.moreira.rm@gmail.com>
2019-08-13 22:42:13 +0300

    fixing bug

--- static/sw.js
+++ static/sw.js
@@ -2,32 +2,36 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/164203f2aaef09e3513c.js",
-    "revision": "42cb5418e2dc88fd7c5a835c311a81fa"
+    "url": "/_nuxt/07c7f1735204e2f7c169.js",
+    "revision": "1a128ea7e5bf369d0d04b52026d94605"
   },
   {
-    "url": "/_nuxt/1764adc3b2d48f626bdc.js",
-    "revision": "60b1c74feef2b4a7996d54ce5b32a1ce"
+    "url": "/_nuxt/0ac8f8e729025fa5aaf7.js",
+    "revision": "7632af0cfd7db8dfbeae58860dffd082"
   },
   {
-    "url": "/_nuxt/951c7fa30bcc905d9cf3.js",
-    "revision": "966e6a67f35f0bd02d22f5d5fc7737d5"
+    "url": "/_nuxt/256cd6f3c000fe1c66ab.js",
+    "revision": "53d92ea300d5a9e5a67d2e71631d9cf7"
   },
   {
-    "url": "/_nuxt/9fd7ef8b630593b3c8a1.js",
-    "revision": "3f6a04ad4d0e7db4fced03cbb9d07ea0"
+    "url": "/_nuxt/409a1dd5804a4d9cff4c.js",
+    "revision": "8ad16510b1595edebb575759b8f5a8e2"
   },
   {
-    "url": "/_nuxt/c52b8ce28b2aa0381c56.js",
-    "revision": "32a5a183e24a7c75d16c56973a05c657"
+    "url": "/_nuxt/4d410f1b0790a6700447.js",
+    "revision": "036b1091323f869eca170cf82e9c7b5f"
   },
   {
-    "url": "/_nuxt/d3bf67c33365cc938028.js",
-    "revision": "190d5fe55731c685584f77c56a3bf16e"
+    "url": "/_nuxt/590f0363ce4ce957caad.js",
+    "revision": "a8eb7f05014ac06b9f5bc061376b85d7"
   },
   {
-    "url": "/_nuxt/d68a4b1c65df13481a1c.js",
-    "revision": "e401cc1fd3d250292f44520098819ed1"
+    "url": "/_nuxt/c0135ad8bf236ebaf14d.js",
+    "revision": "21cf76dcd177924b9847f5b0489fc4ff"
+  },
+  {
+    "url": "/_nuxt/f90283949d8b88e93aa0.js",
+    "revision": "7b514a5f26ef624000f6d0103660773f"
   }
 ], {
   "cacheId": "nuxt-netlifycms-boilerplate",
