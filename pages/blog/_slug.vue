a6011d9bfcf60e295f5f1a100be61fe874a18bff
mugas <ricardo.d.moreira.rm@gmail.com>
2021-10-20 21:11:37 +0300

    change metadata

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -115,7 +115,7 @@ export default {
         {
           hid: 'og:title',
           property: 'og:title',
-          content: `${this.title} | Code Tribe`,
+          content: `${this.title} | Ricardo Moreira`,
         },
         {
           hid: 'description',
@@ -160,7 +160,7 @@ export default {
         {
           hid: 'twitter:site',
           name: 'twitter:site',
-          content: '@tribe_code',
+          content: '@moreira_creates',
         },
         {
           hid: 'twitter:description',
@@ -170,7 +170,7 @@ export default {
         {
           hid: 'twitter:image',
           name: 'twitter:image',
-          content: `https://code-tribe.com${this.thumbnail}`,
+          content: `https://ricardomoreira.io/${this.thumbnail}`,
         },
       ],
     }
